51b5bb1368f3ead0819d07eb9a3bab74f052a8d4
Agustin <agustinsanchezx@gmail.com>
2022-04-21 20:29:40 -0300

    add proyecto

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="titulo">
+      <p className="primera-p">Por favor, scrollee hacia abajo!</p>
+      <p className="segunda-p">(mantenga la pantalla completa)</p>
+      <h1>React no te tengo miedo!</h1>
+      <h2>:)</h2>
+    </div>
+
+  );
+}
+
+export default App;
